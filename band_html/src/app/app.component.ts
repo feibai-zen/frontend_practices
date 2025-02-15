8798126f56f9f28d40a3cabb4676f73582b7acdb
Liyuanlong <959769993@qq.com>
2025-02-15 17:39:58 +0800

    add html databand

---
+++ band_html/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Myc01Component } from './myc01/myc01.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Myc01Component],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'band_html';
+}
---
+++ band_html/src/app/myc01/myc01.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myc01',
+  imports: [],
+  templateUrl: './myc01.component.html',
+  styleUrl: './myc01.component.css'
+})
+export class Myc01Component {
+  uname = 'feibai';
+  age = 20
+}
