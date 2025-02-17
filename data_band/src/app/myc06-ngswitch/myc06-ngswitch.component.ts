f8ce515c1bbc5570843d3703b3f8887a508c6ef8
Liyuanlong <959769993@qq.com>
2025-02-17 21:34:34 +0800

    ngswitch

--- data_band/src/app/myc06-ngswitch/myc06-ngswitch.component.ts
+++ data_band/src/app/myc06-ngswitch/myc06-ngswitch.component.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-myc06-ngswitch',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './myc06-ngswitch.component.html',
   styleUrl: './myc06-ngswitch.component.css'
 })
 export class Myc06NgswitchComponent {
+  // userLevel = 'normal';
+  // userLevel = 'vip';
+  // userLevel = 'blackgold';
+  userLevel = 'gold';
+
 
 }
