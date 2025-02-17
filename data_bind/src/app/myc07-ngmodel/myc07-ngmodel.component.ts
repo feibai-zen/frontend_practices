09fddcdef48a37244811e0c2cf485bdbef00b051
Liyuanlong <959769993@qq.com>
2025-02-17 23:23:55 +0800

    ngModel demo

--- data_bind/src/app/myc07-ngmodel/myc07-ngmodel.component.ts
+++ data_bind/src/app/myc07-ngmodel/myc07-ngmodel.component.ts
@@ -10,9 +10,21 @@ import { FormsModule } from '@angular/forms';
 export class Myc07NgmodelComponent {
   uname = 'dingding';
   userName = ''
+  testUserName = ''
+  tips_msg = ''
 
   nameChangeProcessor() {
     console.log(this.userName)
   }
 
+  testUserNameChanging() {
+    if (this.testUserName.length <= 3) {
+      this.tips_msg = '用户密码长度必须大于3'
+    } else if (this.testUserName.length > 9) {
+      this.tips_msg = '用户密码长度必须小于9'
+    } else {
+      this.tips_msg = '用户密码长度合法'
+    }
+
+  }
 }
